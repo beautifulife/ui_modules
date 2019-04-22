b6cff738c7f943e650e77f2934b03d77fb791772
Beautifulife <davidkim1018@gmail.com>
2019-04-22 16:20:17 +0900

    Make boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          welcome
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
